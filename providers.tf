87785399c4af22e5ca373fd44ff42ead7311f495
Samarth <samarth38work@gmail.com>
2025-07-13 11:44:35 +0000

    initializing-proj

---
+++ providers.tf
@@ -0,0 +1,15 @@
+locals {
+    name = "organization-name"
+    org_short_name = "org-short-name"
+
+    region = "ap-south-2"
+
+    environment = "production"
+    organization_id = "org-1234567890"
+    billing_account = "billing-1234567890"
+}
+
+provider "github" {
+    owner = local.org_short_name
+    token = "<YOUR_GITHUB_TOKEN>"
+}
---
+++ versions.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_version = ">= 1.0.0"
+    
+    required_providers {
+        github = {
+            source  = "integrations/github"
+            version = "~> 6.6.0"
+        }
+    }
+    backend "local" {
+        path = "terraform.tfstate"
+    }
+    # backend "aws-s3" {
+    #     bucket         = "my-terraform-state-bucket"
+    #     key            = "terraform.tfstate"
+    #     region         = "us-west-2"
+    #     encrypt        = true
+    #     dynamodb_table = "terraform-locks"
+    # }
+}
